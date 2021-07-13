d9cc3115d3f30f2ace06d0d69926f7df8d4632db
majdalden <majd.alden.h@gmail.com>
2021-07-13 11:36:53 +0300

    Bugs Fix

--- sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/decorators/RangeDayDecorator.java
+++ sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/decorators/RangeDayDecorator.java
@@ -2,11 +2,15 @@ package com.prolificinteractive.materialcalendarview.sample.decorators;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
+
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.sample.R;
+
 import java.util.HashSet;
 
 /**
@@ -14,29 +18,29 @@ import java.util.HashSet;
  */
 public class RangeDayDecorator implements DayViewDecorator {
 
-  private final HashSet<CalendarDay> list = new HashSet<>();
-  private final Drawable drawable;
+    private final HashSet<CalendarDay> list = new HashSet<>();
+    private final Drawable drawable;
 
-  public RangeDayDecorator(final Context context) {
-    drawable = context.getResources().getDrawable(R.drawable.my_selector);
-  }
+    public RangeDayDecorator(final Context context) {
+        drawable = ContextCompat.getDrawable(context, R.drawable.my_selector);
+    }
 
-  @Override
-  public boolean shouldDecorate(CalendarDay day) {
-    return list.contains(day);
-  }
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return list.contains(day);
+    }
 
-  @Override
-  public void decorate(DayViewFacade view) {
-    view.setSelectionDrawable(drawable);
-  }
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.setSelectionDrawable(drawable);
+    }
 
-  /**
-   * We're changing the dates, so make sure to call {@linkplain MaterialCalendarView#invalidateDecorators()}
-   */
-  public void addFirstAndLast(final CalendarDay first, final CalendarDay last) {
-    list.clear();
-    list.add(first);
-    list.add(last);
-  }
+    /**
+     * We're changing the dates, so make sure to call {@linkplain MaterialCalendarView#invalidateDecorators()}
+     */
+    public void addFirstAndLast(final CalendarDay first, final CalendarDay last) {
+        list.clear();
+        list.add(first);
+        list.add(last);
+    }
 }
